ff9fdf70d50ec5e86e8974bfb1e7ad1be00b78b4
Astitva21 <astitvajain4@gmail.com>
2026-08-26 19:08:45 +0530

    Create is_prime function for prime number calculation
    Refactored prime number generation into a function.

--- q3.py
+++ q3.py
@@ -1,13 +1,13 @@
-
-prime = []
-p = int(input("Enter a number:"))
-print(p)
-for i in range(2,p+1):
-    for j in range(2,i):
-        if i%j == 0:
-            break
-    else:
-        prime.append(i)
-
-print(prime)
+def is_prime(n):
+    prime = []
+    p = int(input("Enter a number:"))
+    print(p)
+    for i in range(2,p+1):
+        for j in range(2,i):
+            if i%j == 0:
+                break
+        else:
+            prime.append(i)
+    
+    print(prime)
 #as the else syntax is written after loop it will work only if loop is competly executed and not broken by (in this case) break 
