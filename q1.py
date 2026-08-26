653bd1abf02f23b0d563e715c056206763c4c825
Astitva21 <astitvajain4@gmail.com>
2026-08-26 18:51:30 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,50 @@
+import random
+num = []
+
+
+#number allocation
+n = int(input("Enter the number of numbers:"))
+for i in range (0,n):
+    a = random.randint(0,10)
+    num.append(a)
+
+
+#for sum of numbers
+sum = 0
+for i in range(0,n):
+    sum += num[i]
+print(f"Sum is :",sum)
+
+
+#for maximum
+max = 0
+for i in range(0,n):
+    if max < num[i]:
+        max = num[i]
+print(num)
+print(f"Max is:",max)
+
+
+#for minimum
+min = max
+for i in range(0,n):
+    if min > num[i]:
+        min = num[i]
+print(f"Min is:",min)
+
+
+#for number of even and odd
+even = 0
+odd = 0
+for i in range(0,n):
+    if num[i]%2 == 0:
+        even+=1
+    else:
+        odd+=1
+print(f"Number of odd numbers:",odd)
+print(f"Number of even numbers:",even)
+
+
+#revrse of list
+num.reverse()
+print(num)
